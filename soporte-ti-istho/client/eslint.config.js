4c1587a3d77b5b581c948cdb0781de5a242ea15f
Osman Gallego <114951764+gallego2022@users.noreply.github.com>
2026-08-05 09:10:18 -0500

    chore: agregar eslint-plugin-jsx-a11y con severidad error en componentes corregidos, warn en el resto

--- soporte-ti-istho/client/eslint.config.js
+++ soporte-ti-istho/client/eslint.config.js
@@ -2,7 +2,20 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import jsxA11y from 'eslint-plugin-jsx-a11y'
 import { defineConfig, globalIgnores } from 'eslint/config'
+
+const A11Y_STRICT_FILES = [
+  'src/components/common/Modal.jsx',
+  'src/components/common/ConfirmDialog.jsx',
+  'src/components/common/Select.jsx',
+  'src/components/common/DatePicker.jsx',
+  'src/components/common/Pagination.jsx',
+]
+
+const a11yRuleNames = Object.keys(jsxA11y.flatConfigs.recommended.rules)
+const a11yWarnRules = Object.fromEntries(a11yRuleNames.map((rule) => [rule, 'warn']))
+const a11yErrorRules = Object.fromEntries(a11yRuleNames.map((rule) => [rule, 'error']))
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -12,10 +25,16 @@ export default defineConfig([
       js.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      jsxA11y.flatConfigs.recommended,
     ],
     languageOptions: {
       globals: globals.browser,
       parserOptions: { ecmaFeatures: { jsx: true } },
     },
+    rules: a11yWarnRules,
+  },
+  {
+    files: A11Y_STRICT_FILES,
+    rules: a11yErrorRules,
   },
 ])
